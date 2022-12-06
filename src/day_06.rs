02fa7794b6525a7271529ab0e13fc78962c67ff8
Antoine Pultier <45740+fungiboletus@users.noreply.github.com>
2022-12-06 19:42:18 +0100

    ⛷️ Day 6 part 2

--- src/day_06.rs
+++ src/day_06.rs
@@ -26,7 +26,24 @@ pub fn day_6_part_1(data: &str) -> usize {
 }
 
 pub fn day_6_part_2(data: &str) -> usize {
-    return 42;
+    let characters: Vec<char> = data.chars().collect();
+
+    // This time with 14 characters
+
+    'outer: for i in 14..characters.len() {
+        let mut hit_table = [false; 26];
+        for j in i - 14..i {
+            let char_j = characters.get(j).expect("Unable to get character");
+            let char_j_index = *char_j as usize - 97;
+            if hit_table[char_j_index] {
+                // Continue outer loop
+                continue 'outer;
+            }
+            hit_table[char_j_index] = true;
+        }
+        return i;
+    }
+    panic!("Unable to find a solution");
 }
 
 #[cfg(test)]
@@ -50,6 +67,10 @@ mod tests {
 
     #[test]
     fn test_day_6_part_2() {
-        assert_eq!(day_6_part_2(EXAMPLE_A), 42);
+        assert_eq!(day_6_part_2(EXAMPLE_A), 19);
+        assert_eq!(day_6_part_2(EXAMPLE_B), 23);
+        assert_eq!(day_6_part_2(EXAMPLE_C), 23);
+        assert_eq!(day_6_part_2(EXAMPLE_D), 29);
+        assert_eq!(day_6_part_2(EXAMPLE_E), 26);
     }
 }
